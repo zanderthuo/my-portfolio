02520626f34fc922d4fafb85c3d8cf0ef7d4d29e
Alexander Thuo <thuozander@gmail.com>
2022-05-24 21:16:41 +0300

    Update ResumeContent.js

--- src/components/Resume/ResumeContent.js
+++ src/components/Resume/ResumeContent.js
@@ -6,12 +6,9 @@ function ResumeContent(props) {
       <h5 className={props.title ? "resume-title" : "resume-no-title"}>
         {props.title}
       </h5>
-<<<<<<< HEAD
-=======
       <h5>
         {props.employer}
       </h5>
->>>>>>> ffab9099c6da21b57ba96ea8513477de04028957
       <p>
         <em>{props.date}</em>
       </p>
